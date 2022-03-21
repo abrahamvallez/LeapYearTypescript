df1742c13718b550752b214aa4d97c0bbde71c9f
Abraham Vallez <abevallez@gmail.com>
2022-03-21 22:45:46 +0100

    refactor ifs

--- src/LeapYear.ts
+++ src/LeapYear.ts
@@ -1,11 +1,8 @@
 
 export const isLeapYear = (year: number): boolean => {
-    if(year % 4 == 0 && year % 100 != 0) {
+    if(year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
         return true
     }
-    if(year % 400 == 0) {
-        return true
-    }
-    
+
     return false
 }
